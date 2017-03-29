6ad99db7634fa4b980263f08fd806eac7cca6594
ELEON <ELEON>
2017-03-28 20:52:27 -0500

    portfolio

--- script/jquery.js
+++ script/jquery.js
@@ -2,5 +2,10 @@ $(document).ready(function(){
   $('#myWork').click(function(){
     $('#work-list').toggleClass('noDisplay');
     $('.aboutMe').toggleClass('noDisplay');
+    if ($('#myWork').text() === 'My Work'){
+      $('#myWork').html('About Me');
+    } else {
+      $('#myWork').html('My Work');
+    };
   });
 });
